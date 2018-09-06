3517221ec275f4fa1dd78967fb070ea762832a8a
Qgfsixsixsix <34310226+Qgfsixsixsix@users.noreply.github.com>
2018-09-06 11:49:34 +0800

    Create one.java

---
+++ SwordOffer/one.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int NumberOf1(int n) {
+        int count = 0;
+        while(n != 0){
+            n = n & (n-1);
+            count++;
+        }
+        return count;
+    }
+}
+
+求二进制(binary system)1的个数
+
+计算机中的负数是以其补码形式存在的 补码=原码取反+1。
+先把整数原码取反
+
